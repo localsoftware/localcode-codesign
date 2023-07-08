650dc652519349a64418063c85bc248467d3c4e6
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2023-07-07 18:01:25 -0600

    feat(d3): added right dataset

--- components/DThreeBox.vue
+++ components/DThreeBox.vue
@@ -33,10 +33,12 @@ const formatYield = async (csvPath) => {
 const makeSVG = (farmObject, height, margin, width) => {
   const innerHeight = height * farmObject.farmCrops.length
 
-  // this has to be according to dates...
   const x = d3
     .scaleLinear()
-    .domain([d3.min(farmObject.farmDates), d3.max(farmObject.farmDates) + 1])
+    .domain([
+      new Date(farmObject.farmDates[0]),
+      new Date(farmObject.farmDates[farmObject.farmDates.length - 1]),
+    ])
     .rangeRound([margin.left, width.value - margin.right])
 
   const y = d3
@@ -100,14 +102,12 @@ const makeSVG = (farmObject, height, margin, width) => {
     .selectAll('rect')
     .data((d) => d)
     .join('rect')
-    .attr('x', (d, i) => {
-      console.log(x(farmObject.farmDates[i]), d, x)
-      x(farmObject.farmDates[i]) + 1
+    .attr('x', (d, i) => x(new Date(farmObject.farmDates[i])) + 1)
+    .attr('width', (d, i) => {
+      const currDate = new Date(farmObject.farmDates[i])
+      currDate.setDate(currDate.getDate() + 1)
+      return x(currDate) - x(new Date(farmObject.farmDates[i])) - 1
     })
-    .attr(
-      'width',
-      (d, i) => x(farmObject.farmDates[i] + 1) - x(farmObject.farmDates[i]) - 1
-    )
     .attr('height', y.bandwidth() - 1)
     .attr('fill', (d) => (isNaN(d) ? '#eee' : d === 0 ? '#fff' : color(d)))
     .append('title')
@@ -125,102 +125,18 @@ onMounted(async () => {
   // const wagner = await d3.csv('csv/2022_Wagner.csv')
 
   const bayViewData = await formatYield('csv/2022_Bayview.csv')
-  console.log(bayViewData, 44444)
-
-  const names = [
-    'Alaska',
-    'Ala.',
-    'Ark.',
-    'Ariz.',
-    'Calif.',
-    'Colo.',
-    'Conn.',
-    'D.C.',
-    'Del.',
-    'Fla.',
-    'Ga.',
-    'Hawaii',
-    'Iowa',
-    'Idaho',
-    'Ill.',
-    'Ind.',
-    'Kan.',
-    'Ky.',
-    'La.',
-    'Mass.',
-    'Md.',
-    'Maine',
-    'Mich.',
-    'Minn.',
-    'Mo.',
-    'Miss.',
-    'Mont.',
-    'N.C.',
-    'N.D.',
-    'Neb.',
-    'N.H.',
-    'N.J.',
-    'N.M',
-    'Nev.',
-    'N.Y.',
-    'Ohio',
-    'Okla.',
-    'Ore.',
-    'Pa.',
-    'R.I.',
-    'S.C.',
-    'S.D.',
-    'Tenn.',
-    'Texas',
-    'Utah',
-    'Va.',
-    'Vt.',
-    'Wash.',
-    'Wis.',
-    'W.Va.',
-    'Wyo.',
-  ]
 
   const margin = { top: 20, right: 1, bottom: 40, left: 40 }
   const height = 16
-  const innerHeight = height * names.length
 
-  const values = []
-  const year0 = d3.min(data[0].data.values.data, (d) => d[0])
-  const year1 = d3.max(data[0].data.values.data, (d) => d[0])
-  const years = d3.range(year0, year1 + 1)
-  for (const [year, i, value] of data[0].data.values.data) {
-    if (value == null) continue
-    ;(values[i] || (values[i] = []))[year - year0] = value
-  }
-  const year = data[0].data.chart_options.vaccine_year
-  console.log(bayViewData.farmDates[bayViewData.farmDates.length - 1], 'jjjjjj')
-
-  console.log(values)
-  console.log(makeSVG(bayViewData, height, margin, width), 'svggggggggg')
-  const x = d3
-    .scaleLinear()
-    .domain([d3.min(years), d3.max(years) + 1])
-    .rangeRound([margin.left, width.value - margin.right])
-
-  const y = d3
-    .scaleBand()
-    .domain(names)
-    .rangeRound([margin.top, margin.top + innerHeight])
-
-  const color = d3.scaleSequentialSqrt(
-    [0, d3.max(values, (d) => d3.max(d))],
-    d3.interpolatePuRd
-  )
-
-  const xAxis = (g) =>
-    g.call((g) =>
-      g
-        .append('g')
-        .attr('transform', `translate(0,${margin.top})`)
-        .call(d3.axisTop(x).ticks(null, 'd'))
-        .call((g) => g.select('.domain').remove())
-    )
+  // const xAxis = (g) =>
+  //   g.call((g) =>
+  //     g
+  //       .append('g')
+  //       .attr('transform', `translate(0,${margin.top})`)
+  //       .call(d3.axisTop(x).ticks(null, 'd'))
+  //       .call((g) => g.select('.domain').remove())
+  // )
   // .call((g) =>
   //   g
   //     .append('g')
@@ -243,56 +159,7 @@ onMounted(async () => {
   //     .call((g) => g.select('.domain').remove())
   // )
 
-  const yAxis = (g) =>
-    g
-      .attr('transform', `translate(${margin.left},0)`)
-      .call(d3.axisLeft(y).tickSize(0))
-      .call((g) => g.select('.domain').remove())
-
-  const f = d3.format(',d')
-  const format = (d) =>
-    isNaN(d)
-      ? 'N/A cases'
-      : d === 0
-      ? '0 cases'
-      : d < 1
-      ? '<1 case'
-      : d < 1.5
-      ? '1 case'
-      : `${f(d)} cases`
-
-  const svg = d3
-    .create('svg')
-    .attr('viewBox', [
-      0,
-      0,
-      width.value,
-      innerHeight + margin.top + margin.bottom,
-    ])
-    .attr('font-family', 'sans-serif')
-    .attr('font-size', 10)
-
-  svg.append('g').call(xAxis)
-
-  svg.append('g').call(yAxis)
-
-  svg
-    .append('g')
-    .selectAll('g')
-    .data(values)
-    .join('g')
-    .attr('transform', (d, i) => `translate(0,${y(names[i])})`)
-    .selectAll('rect')
-    .data((d) => d)
-    .join('rect')
-    .attr('x', (d, i) => x(years[i]) + 1)
-    .attr('width', (d, i) => x(years[i] + 1) - x(years[i]) - 1)
-    .attr('height', y.bandwidth() - 1)
-    .attr('fill', (d) => (isNaN(d) ? '#eee' : d === 0 ? '#fff' : color(d)))
-    .append('title')
-    .text((d, i) => `${format(d)} per 100,000 people in ${years[i]}`)
-
-  container.value.append(svg.node())
+  container.value.append(makeSVG(bayViewData, height, margin, width).node())
 })
 </script>
 
